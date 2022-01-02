c2118d8894c0663cc99ddf98b45630d1e2277ed2
lhdong123 <lyhandong123@gmail.com>
2022-01-02 12:33:24 +0700

    test

--- components/User/mailer.js
+++ components/User/mailer.js
@@ -1,5 +1,18 @@
 const nodemailer = require("nodemailer");
+const { google } = require('googleapis');
 
+// These id's and secrets should come from .env file.
+const CLIENT_ID = '660685950439-oskm4ugkok2lo30m0h22p7k0ifadrrhu.apps.googleusercontent.com';
+const CLEINT_SECRET = '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';
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+const REFRESH_TOKEN = '1//04eqKRVvWWxSQCgYIARAAGAQSNwF-L9Irk5ph541Qs6Djs_vNF8FAm604nLgeMqqCMXh8eQUEE6T9Il9VDA7yMOXi1ruXXAT9mMQ';
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLEINT_SECRET,
+  REDIRECT_URI
+);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 
 const saltRounds = 10;
@@ -19,17 +32,19 @@ async function configEmailToSend (account, OTP) {
 }
 
 async function configEmailToSend1 (account, OTP) {
-    const smtpTransport = nodemailer.createTransport(
-        {
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            auth: {
-                user: 'ptudwnc.classroom@gmail.com',
-                pass: 'hcmusk18@ddl'
-            }
-        }
-        );
+    const accessToken = await oAuth2Client.getAccessToken();
+
+    const smtpTransport = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: 'ptudwnc.classroom@gmail.com',
+        clientId: CLIENT_ID,
+        clientSecret: CLEINT_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accessToken: accessToken,
+      },
+    });
     const mail = {
         from: "ptudwnc.classroom@gmail.com",
         to: account,
